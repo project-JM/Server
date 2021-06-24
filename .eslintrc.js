e1351fbe0f7e1eacf945666e8486dd4f95504cca
vagabondms <hiseokseok@gmail.com>
2021-06-25 00:05:56 +0900

    :tada: 자 이제시작이야 내꿈을
    내 꿈을 위한 여행 삐가츄

---
+++ .eslintrc.js
@@ -0,0 +1,20 @@
+module.exports = {
+  env: {
+    browser: true,
+    commonjs: true,
+    es2021: true,
+  },
+  extends: [
+    'airbnb-base',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:prettier/recommended',
+    'prettier/@typescript-eslint',
+  ],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    project: './tsconfig.json',
+    ecmaVersion: 2021,
+    sourceType: 'module',
+  },
+  plugins: ['@typescript-eslint'],
+};
---
+++ src/app.ts
@@ -0,0 +1,9 @@
+import express from 'express';
+
+const app = express();
+
+const port: number = 3000;
+
+app.listen(port, () => {
+  console.log('hi');
+});
