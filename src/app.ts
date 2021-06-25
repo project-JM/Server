a185a6a7e6450c61c87b7143ee4480fa93aef661
vagabondms <hiseokseok@gmail.com>
2021-06-25 18:08:17 +0900

    :sparkles: cors, morgan, body-parser 설정

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,19 @@
 import express from 'express';
+import morgan from 'morgan';
+import cors from 'cors';
 
-const app = express();
-
+const app: express.Application = express();
 const port = 3000;
 
+app.use(morgan('dev'));
+
+const corsOption = {
+  origin: '*',
+};
+
+app.use(cors(corsOption));
+app.use(express.json());
+
 app.listen(port, () => {
-  console.log('hi');
+  console.log(`listening on port ${port}`);
 });
